46384d73d34910940e9c5af00d5e06bd937e93bc
25f3001999-hue <25f3001999@ds.study.iitm.ac.in>
2026-05-16 12:15:52 +0530

    changes in urls

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,6 @@ app.use(cors({
     },
     credentials: true,
 }));
-app.options(/(.*)/, cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
