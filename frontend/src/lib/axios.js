2ef26aaa8807fdb4f68712031fa329aa64e1309a
25f3001999-hue <25f3001999@ds.study.iitm.ac.in>
2026-05-17 00:59:46 +0530

    changes in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config({path: "../../.env"});
+console.log(import.meta.env.VITE_API_BASE_URL);
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
