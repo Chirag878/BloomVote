0ea3c231c8200d2230de8d53c5d0655c42c50228
25f3001999-hue <25f3001999@ds.study.iitm.ac.in>
2026-05-17 14:13:31 +0530

    imported browserrouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ToastProvider } from "./context/ToastContext.jsx";
 import { HashRouter } from "react-router-dom";
